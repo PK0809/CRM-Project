eebba81ee5a4c2f06ee1fd6a2a7f32a93ca2e7fe
PK0809 <nagarajnadahrts33@gmail.com>
2025-09-06 14:13:20 +0530

    Change-0609

--- crm/context_processors.py
+++
@@ -1,8 +0,0 @@
-
-import os
-from django.conf import settings
-
-def global_logo_path(request):
-    return {
-        'logo_path': os.path.join(settings.BASE_DIR, 'static', 'images', 'logo.png')
-    }
